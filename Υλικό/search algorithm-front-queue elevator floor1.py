0300e857e67e02e8d30cebeebd3daf589437f516
Frosso Varsou <frossovarsou@gmail.com>
2023-12-01 21:57:48 +0200

    Update Υλικό

--- Υλικό/search algorithm-front-queue elevator floor1.py
+++ Υλικό/search algorithm-front-queue elevator floor1.py
@@ -20,8 +20,8 @@ sys.setrecursionlimit(10**6)
   
 def go_to_floor1(state):
     if state[-1]<8 and state[1]>0:
-        if state[1]>10-state[-1]:
-            new_state = [1] + [state[1] + state[-1] - 10] + [state[2]] + [state[3]] + [state[4]] + [8]
+        if state[1]>8-state[-1]:
+            new_state = [1] + [state[1] + state[-1] - 8] + [state[2]] + [state[3]] + [state[4]] + [8]
         else:
             new_state = [1] + [0] + [state[2]] + [state[3]] + [state[4]] + [state[1] + state[-1]]
         return new_state
